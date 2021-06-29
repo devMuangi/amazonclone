64931c757f398ea71363c674436b6bc20677eef2
devMuangi <devmuangi@gmail.com>
2021-06-29 14:33:27 +0300

    some config and tweaks

--- src/App.js
+++ src/App.js
@@ -1,13 +1,51 @@
-import React from "react";
+import React, {useState, useEffect }from "react";
 import "./App.css";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./components/Home";
 import Header from "./components/Header";
+import Login from "./components/Login";
+import { useStateValue } from "./components/StateProvider";
+import { auth } from "./firebase";
+
+
+
+
 function App() {
+
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  useEffect(() => {
+    // will only run once when the app component loads...
+
+    auth.onAuthStateChanged((authUser) => {
+      console.log("THE USER IS >>> ", authUser);
+
+      if (authUser) {
+        // the user just logged in / the user was logged in
+
+        dispatch({
+          type: "SET_USER",
+          user: authUser,
+        });
+      } else {
+        // the user is logged out
+        dispatch({
+          type: "SET_USER",
+          user: null,
+        });
+      }
+    });
+  }, []);
+
+
+
 return (
   <div className="app">
     <Router>
       <Switch>
+      <Route path="/login">
+  <Login />
+</Route>
         <Route path="/">
           <Header/>
           <Home />
--- src/components/Header.js
+++ src/components/Header.js
@@ -3,16 +3,27 @@ import React from "react";
 import "./Header.css";
 import SearchIcon from "@material-ui/icons/Search";
 import ShoppingBasketIcon from "@material-ui/icons/ShoppingBasket";
+import { Link } from "react-router-dom";
+import { useStateValue } from "./StateProvider";
+import { auth } from "../firebase";
 
-function Header() {  
+function Header() {
+  const [{ basket, user }, dispatch] = useStateValue();
+
+  const handleAuthenticaton = () => {
+    if (user) {
+      auth.signOut();
+    }
+  }
+
   return (
     <div className="header">
-      
+      <Link to="/">
         <img
           className="header__logo"
           src="http://pngimg.com/uploads/amazon/amazon_PNG11.png"
         />
-      
+      </Link>
 
       <div className="header__search">
         <input className="header__searchInput" type="text" />
@@ -20,19 +31,19 @@ function Header() {
       </div>
 
       <div className="header__nav">
-        
-          <div className="header__option">
-            <span className="header__optionLineOne">Hello Guest</span>
-            <span className="header__optionLineTwo">Sign In</span>
+        <Link to={!user && '/login'}>
+          <div onClick={handleAuthenticaton} className="header__option">
+            <span className="header__optionLineOne">Hello {!user ? 'Guest' : user.email}</span>
+            <span className="header__optionLineTwo">{user ? 'Sign Out' : 'Sign In'}</span>
           </div>
-        
+        </Link>
 
-        
+        <Link to='/orders'>
           <div className="header__option">
             <span className="header__optionLineOne">Returns</span>
             <span className="header__optionLineTwo">& Orders</span>
           </div>
-        
+        </Link>
         
 
         <div className="header__option">
@@ -40,14 +51,14 @@ function Header() {
           <span className="header__optionLineTwo">Prime</span>
         </div>
 
-        
+        <Link to="/checkout">
           <div className="header__optionBasket">
             <ShoppingBasketIcon />
             <span className="header__optionLineTwo header__basketCount">
-              0
+              {basket?.length}
             </span>
           </div>
-        
+        </Link>
       </div>
     </div>
   );
